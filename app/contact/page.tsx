927e26e5bbfd48168d2ef6cc7e90ecd552769bfa
David Bautista <dj.bautista10@uniandes.edu.co>
2024-09-06 14:47:01 -0500

    Fixed mobile layout for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,29 +15,29 @@ export default function ContactPage() {
       <main
         className={twMerge([
           'mt-24',
-          'mx-auto px-6 py-12 md:py-24',
+          'mx-auto p-6 md:py-24',
           'flex-col items-center',
           'text-neutral-50',
         ])}
       >
-        <Section className="mx-auto w-full max-w-6xl gap-16 md:gap-24">
-          <div className="flex w-full flex-1 flex-col justify-between">
+        <Section className="mx-auto w-full max-w-6xl gap-12 md:gap-16 md:gap-24">
+          <div className="flex w-full flex-1 flex-col justify-between gap-12">
             <div>
               <Typography
                 as="h1"
                 className={twMerge([
                   silkscreen.className,
-                  'text-4xl md:min-h-16 md:text-5xl',
+                  'min-h-9 text-3xl md:min-h-16 md:text-5xl',
                 ])}
               >
                 Let&apos;s Connect
               </Typography>
-              <p className="text-lg text-neutral-200">
+              <p className="text-sm text-neutral-200 md:text-lg">
                 I&apos;m always open to new opportunities and collaborations.
                 Feel free to reach out!
               </p>
             </div>
-            <div className="space-y-4">
+            <div className="space-y-4 text-xs md:text-sm">
               <Link
                 href="mailto:david@davidbautista.co"
                 className="flex w-fit items-center space-x-2 text-primary-200 hover:text-primary"
@@ -67,7 +67,7 @@ export default function ContactPage() {
               </Link>
             </div>
 
-            <div className="mt-4 flex w-fit flex-col space-y-4">
+            <div className="flex w-fit flex-col space-y-4 md:mt-4">
               <Link
                 href="/davidbautista.pdf"
                 target="_blank"
@@ -93,12 +93,12 @@ export default function ContactPage() {
               back to you as soon as possible.
             </p>
           </div>
-          <div className="w-full max-w-lg space-y-4">
-            <ContactForm />
-            <p className="px-2 text-sm text-neutral-300 md:hidden">
+          <div className="flex w-full max-w-lg flex-col gap-4">
+            <p className="px-2 text-xs text-neutral-400 md:hidden">
               Prefer to share your email? Fill out the form and I&apos;ll get
               back to you as soon as possible.
             </p>
+            <ContactForm />
           </div>
         </Section>
       </main>
