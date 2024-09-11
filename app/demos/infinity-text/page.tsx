1e64e8d8d9276cee83b19f5f561d58f9494ffcf4
David Bautista <dj.bautista10@uniandes.edu.co>
2024-09-10 23:18:39 -0500

    Added title in infinity text demo

--- app/demos/infinity-text/page.tsx
+++ app/demos/infinity-text/page.tsx
@@ -6,7 +6,8 @@ export default function InfinityTextPage() {
   return (
     <main className="flex h-screen flex-col items-center justify-center bg-black text-white">
       <Section className="relative h-full items-center justify-center px-0 pb-0">
-        <div className="absolute top-0 flex w-full flex-col items-center justify-center gap-8 p-8 text-center">
+        <div className="absolute left-0 top-0 z-10 h-screen w-screen bg-gradient-to-b from-black/75 to-transparent" />
+        <div className="absolute top-0 z-20 flex w-full flex-col items-center justify-center gap-8 p-8 text-center">
           <h1 className="text-7xl font-bold text-white">
             Infinity Text Banner
           </h1>
